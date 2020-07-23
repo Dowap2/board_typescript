1d97404361a5eaf3aa6a8700d913d328bb010de0
dowap2 <dowapdowari@gmail.com>
2020-07-23 20:58:32 +0900

    fixed like and bad function

--- src/DetailPost.tsx
+++ src/DetailPost.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React , {useState} from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom'
 
 function DetailPost({location}) {
     const info:{title: string , main: string, _id:string , time:string, like:Number , bad:Number} = location.state;
+    const [like , setLike] = useState<Number>(info.like)
+    const [bad , setBad] = useState<Number>(info.bad)
 
-    function likeFunc(like ,bad){
-      axios.put('http://localhost:8000/api/like',{params: {_id: info._id}, data:{like: like+1 , bad: bad}})
+    function likeFunc(){
+      setLike(like+1);
+      axios.put('http://localhost:8000/api/like',{params: {_id: info._id}, data:{like: like , bad: bad}})
+      console.log(like , bad)
     }
-    function badFunc(like ,bad){
-      axios.put('http://localhost:8000/api/like',{params: {_id: info._id}, data:{like: like , bad: bad+1}})
+    function badFunc(){
+      setBad(bad+1);
+      axios.put('http://localhost:8000/api/like',{params: {_id: info._id}, data:{like: like , bad: bad}})
     }
     return (
       <div className="detail__post">
@@ -20,8 +25,8 @@ function DetailPost({location}) {
           <div className="detail__mainText">
             {info.main}
             {info.time}
-            <button onClick={e=> likeFunc(info.like , info.bad)}>좋아요:{info.like}</button>
-            <button onClick={e=> badFunc(info.like , info.bad)}>싫어요:{info.like}</button>
+            <button onClick={e=> likeFunc()}>좋아요:{info.like}</button>
+            <button onClick={e=> badFunc()}>싫어요:{info.bad}</button>
           </div>
       </div>
     );
